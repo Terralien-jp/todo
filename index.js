cd32506ec5e1e1db87baedd09a162bda1511d349
Terralien-jp <kotakotaneko@gmail.com>
2022-01-29 21:46:54 +0900

    Fixed3

--- index.js
+++ index.js
@@ -2,30 +2,46 @@ const form = document.getElementById("form");
 const input = document.getElementById("input");
 const ul = document.getElementById("ul");
 
-// const todos = JSON.parse(localStorage.getItem("todos"));
-
-// if (todos) {
-//     todos.forEach(todo => {
-//             add(todo);
-//          });
-// }
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+    todos.forEach((todo) => {
+        add(todo);
+    });
+}
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
     add();
 });
 
-function add() {
+function add(todo) {
     let todoText = input.value;
 
-    // if (todo) {
-    //     todoText = todo;
-    // }
+    if (todo) {
+        todoText = todo.text;
+    }
 
     if (todoText) {
         const li = document.createElement("li");
-        li.innerText = input.value;
-        li.classList.add("list-group-item");
+
+        li.innerText = todoText;
+        li.classList.add("list-group-item")
+
+        if (todo && todo.completed) {
+            li.classList.add("text-decoration-line-through");
+        }
+
+        li.addEventListener("contextmenu", function (event) {
+            event.preventDefault();
+            li.remove();
+            saveData();
+        });
+ 
+        li.addEventListener("click", function () {
+            li.classList.toggle("text-decoration-line-through");
+            saveData();
+        });
+
         ul.appendChild(li);
         input.value = "";
         saveData();
@@ -34,10 +50,13 @@ function add() {
 
 function saveData() {
     const lists = document.querySelectorAll("li");
-    let todos = [];
+    const todos = [];
 
-    lists.forEach(list => {
-        todos.push(list.innerText);
+    lists.forEach((li) => {
+        todos.push({
+            text: li.innerText,
+            completed: li.classList.contains("texxt-decoration-line-through")
+        });
     });
 
     localStorage.setItem("todos", JSON.stringify(todos));
